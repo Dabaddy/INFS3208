8a67d402da5bcb7efe3ba8cb9bb15b81add654fb
daniel abad <daniel@bluechipelectrical.com.au>
2019-10-03 11:04:23 +1000

    CREATED a login and signup page. DOES NOT connect to dbms. ADDED a style guide of the internet. DELETED some unused files. THE PHP file are for referenceing functionality only (Not to be included in final file system.

---
+++ Speech/includes/dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+//$dbServername = "hadoop";
+//$dbUsername = "root";
+//$dbPassword = "";
+//$dbName = "speech";	
+
+//$db = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+//if(!$db) {
+//	die("Connection Failed: ".mysqli_connect_error());
+}
